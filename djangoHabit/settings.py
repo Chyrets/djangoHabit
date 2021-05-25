e116d690e9bed5c484c504240e16e4c3dfdf3d43
chyrets <uladzislawchyrets@gmail.com>
2021-05-25 09:58:29 +0300

    create authy app added login/logout capability

--- djangoHabit/settings.py
+++ djangoHabit/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'multiselectfield',
 
-    'habit'
+    'habit',
+    'authy'
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
