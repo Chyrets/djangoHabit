1e9439a64c25845a6f0556662c2466331cdf4268
chyrets <uladzislawchyrets@gmail.com>
2021-05-25 10:22:54 +0300

    added displaying the main page and base.html

--- habit/urls.py
+++ habit/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+
+from habit import views
+
 urlpatterns = [
+    path('', views.MainPage.as_view(), name='main'),
 ]
--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+from habit.models import Day, Habit
+
+
+class MainPage(View):
+    """
+    This page display tasks for today
+    """
+
+    def get(self, request, *args, **kwargs):
+        tasks = Habit.objects.filter(day__date=datetime.today(), user=request.user)
+
+        context = {
+            'tasks': tasks
+        }
+
+        return render(request, 'habit/main.html', context)
